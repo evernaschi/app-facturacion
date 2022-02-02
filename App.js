87e4a015c583fd69b2501a91dc20cdddf2c2fb43
unknown <e.vernaschi@hotmail.com.ar>
2022-02-02 02:08:20 -0300

    [ADD] Efecto swipe para borrar filas agregado, falta terminar

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView  } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, Pressable, Animated } from 'react-native';
 import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown'
 import { useState } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const AnimatedIcon = Animated.createAnimatedComponent(Icon);
+
+import GmailStyleSwipeableRow from './swipable/GmailStyleSwipeableRow';
 
 const App = () => {
 	const Stack = createNativeStackNavigator();
@@ -24,21 +30,21 @@ const App = () => {
 			<Stack.Screen
 			name="SeleccionCliente"
 			component={SeleccionClienteScreen}
-			options={{ 
+			options={{
 				title: 'Seleccionar Cliente',
 			}}
 			/>
-			<Stack.Screen name="SeleccionDireccion" 
-			component={SeleccionDireccionScreen} 
-			options={{ 
+			<Stack.Screen name="SeleccionDireccion"
+			component={SeleccionDireccionScreen}
+			options={{
 				title: 'Seleccionar Dirección',
-			}}			
+			}}
 			/>
-			<Stack.Screen name="Facturacion" 
-			component={FacturacionScreen} 
-			options={{ 
+			<Stack.Screen name="Facturacion"
+			component={FacturacionScreen}
+			options={{
 				title: 'Facturacion',
-			}}			
+			}}
 			/>
 		</Stack.Navigator>
 	</NavigationContainer>
@@ -99,10 +105,13 @@ const SeleccionDireccionScreen = ({ navigation, route }) => {
 	direcciones = (Array.isArray(direcciones)) ? direcciones : [direcciones];
 	direcciones = direcciones.map(function(obj, index){
 		let rObj = {};
-		rObj['id'] = index;
+		rObj['id'] = index+1;
+		// Uso index+1 porque initial value con 0 no funciona
 		rObj['title'] = obj;
 		return rObj;
 	});
+	// Si tiene una sola dirección la pongo de initialValue, si tiene más de una entonces no pongo ninguna
+	const initialValue = direcciones.length === 1 ? {id:1} : {id:0}
 	return (
 		<View style={styles.containerCentered}>
 			<Text style={styles.centerText}>Seleccionar Dirección para el Cliente:</Text>
@@ -125,7 +134,7 @@ const SeleccionDireccionScreen = ({ navigation, route }) => {
 							width: "100%",
 						}
 					}}
-					initialValue={{ id: 0 }} // or just '2'
+					initialValue={initialValue} // or just '2'
 					onSelectItem={setSelectedItem}
 					dataSet={direcciones}
 					/>
@@ -148,6 +157,11 @@ const SeleccionDireccionScreen = ({ navigation, route }) => {
 };
 
 const FacturacionScreen = ({ navigation, route }) => {
+	const [count, setCount] = useState(1)
+	const [filas, setFilas] = useState([0])
+	const eliminarFila = (key) => {
+		setFilas(filas.filter(i => i !== key));
+	}
 	return (
 		<View style={styles.container}>
 			<ScrollView >
@@ -160,7 +174,24 @@ const FacturacionScreen = ({ navigation, route }) => {
 					<Text style={[styles.centerText, styles.boldText]}>{route.params.direccion.title}</Text>
 				</View>
 				<Encabezado/>
-				<Fila/>
+				{/* { filas.map((i) => <Fila key={i} index={i} eliminarFilaCallback={eliminarFila}/>) } */}
+				<GestureHandlerRootView>
+				{ filas.map((i) =>
+					<GmailStyleSwipeableRow key={i}>
+					<Fila key={i} index={i} eliminarFilaCallback={eliminarFila}/>
+					</GmailStyleSwipeableRow>
+				)}
+				</GestureHandlerRootView>
+				<View style={styles.containerCentered}>
+				<Button
+					title="Nueva Fila"
+					color="#0099ff"
+					onPress={ () => {
+						setFilas([...filas, count]);
+						setCount(count + 1);
+					}}
+					/>
+				</View>
 			</ScrollView>
 		</View>
 	);
@@ -168,39 +199,56 @@ const FacturacionScreen = ({ navigation, route }) => {
 
 const Encabezado = () => {
 	return (
-	<View style={styles.row}>
-			<Text style={[styles.celda, styles.boldText, {textAlign:"center"}]}>PRODUCTO</Text>
-			<Text style={[styles.celda, styles.boldText, {textAlign:"center"}]}>CAJAS</Text>
-			<Text style={[styles.celda, styles.boldText, {textAlign:"center"}]}>UNIDADES</Text>
+	<View style={[styles.row, {borderTopColor:"black",borderBottomWidth:1}]}>
+			<Text style={[styles.celda, styles.boldText, {flex:3}]}>PRODUCTO</Text>
+			<Text style={[styles.celda, styles.boldText]}>CAJAS</Text>
+			<Text style={[styles.celda, styles.boldText]}>UNID</Text>
+			<View style={{flexDirection:"row", justifyContent:"flex-end", flex:1.3}}>
+				<AnimatedIcon name="delete-forever" size={30} color="black" style={[styles.actionIcon, {paddingTop:5}]}/>
+			</View>
 	</View>
 	)
 }
 
-const Fila = () => {
-	const [text, onChangeText] = useState(null);
-	const [number, onChangeNumber] = useState(null);
+const Fila = (props) => {
+	const [text, onChangeText] = useState("");
+	const [cajas, onChangeCajas] = useState("");
+	const [unidades, onChangeUnidades] = useState("");
 	return (
-	<View style={styles.row}>
+	<View style={[styles.row, {borderTopColor:"black",borderBottomWidth:1}]}>
 		<TextInput
-			style={styles.celda}
+			style={[styles.celda, {flex:3}]}
 			onChangeText={onChangeText}
 			placeholder="Ingrese Producto"
 			value={text}
 		/>
 		<TextInput
 			style={styles.celda}
-			onChangeText={onChangeNumber}
-			value={number}
-			placeholder="Ingrese Cajas"
+			onChangeText={onChangeCajas}
+			value={cajas}
+			placeholder="Cajas"
 			keyboardType="numeric"
 			/>
 		<TextInput
 			style={styles.celda}
-			onChangeText={onChangeNumber}
-			value={number}
-			placeholder="Ingrese Cantidad"
+			onChangeText={onChangeUnidades}
+			value={unidades}
+			placeholder="Cant"
 			keyboardType="numeric"
-		/>
+			/>
+		<View style={{flexDirection:"row", justifyContent:"flex-end", flex:1.3}}>
+		<AnimatedIcon
+			name="delete-forever"
+			size={30}
+			color="red"
+			style={[styles.actionIcon, {paddingTop:8}]}
+			/>
+		</View>
+		{/* <View style={[styles.row, styles.celda, {paddingVertical:0}]}> */}
+			{/* <Pressable onPress={() => {props.eliminarFilaCallback(props.index)}} style={styles.deleteButton}>
+				<Text style={{alignSelf:"center", color:"white", fontWeight: 'bold', fontSize:20}}>X</Text>
+			</Pressable> */}
+		{/* </View> */}
 	</View>
 	)
 }
@@ -235,16 +283,36 @@ const styles = StyleSheet.create({
 		marginTop: 20,
 	},
 	celda: {
-		height: 40,
-		margin: 0,
-		borderWidth: 1,
-		padding: 10,
+		height: 45,
+		padding: 5,
 		flex:1,
+		textAlign:"left",
+		textAlignVertical:"center",
 	},
 	row: {
+		// borderWidth: 1,
+		// margin:5,
+		// borderRadius:5,
 		overflow: "hidden",
 		flexDirection: "row",
 		flexWrap: "nowrap",
-		alignContent: "stretch",
+		// alignContent: "stretch",
+		backgroundColor: 'white',
+		justifyContent:"space-between",
 	},
-});
+	deleteButton: {
+		position: 'absolute',
+		right:0,
+		top:0,
+		bottom:0,
+		width:25,
+		backgroundColor:"red",
+		color:"white",
+		textAlign:"center",
+		justifyContent:"center",
+	},
+	actionIcon: {
+		width: 30,
+		marginHorizontal: 10
+	  },
+	});
